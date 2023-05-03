c8df83beff8cb28859d3299e586f9ed138e8e517
Camila Pinheiro <pinheirocamila49800@gmail.com>
2023-05-02 21:53:46 -0300

    attempt to get data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,44 @@
 'use strict'
 
 import './router.js'
+
+const createCard = (planet) => {
+    const li = document.createElement('li')
+
+    const card = document.createElement('planet-card')
+    card.setAttribute('image', `../img/earth-image.jpg`)
+    // card.setAttribute('name', planet.name)
+
+    li.append(card)
+    return li
+}
+
+const loadCards = async () => {
+    const url = `https://api.le-systeme-solaire.net/rest/bodies/`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    let planets = data.bodies;
+    let planetsJson = {};
+    let planetsArray = [];
+
+    const container = document.getElementById("content-card-planets");
+
+    planets.forEach(planet => {
+        if (planet.isPlanet) {
+            planetsArray.push(planet)
+        }
+
+        planetsJson.planets = planetsArray
+    });
+    console.log(planetsArray);
+    console.log(planetsJson);
+    const cards = createCard(planetsJson)
+
+    console.log(cards);
+
+    container.replaceChildren(...cards);
+};
+
+loadCards();
--- js/router.js
+++ js/router.js
@@ -1,15 +1,16 @@
 'use strict'
 
 const routes = {
-    "/": "/pages/index.html",
-    '/planets': '/pages/planets.html',
-    '/sun': '/pages/sun.html'
+    "/": "/index.html",
+    '/sun': 'pages/sun.html',
+    '/planets': 'pages/planets.html'
+    
 }
 
 const route = async () => {
     window.event.preventDefault()
     window.history.pushState({}, "", window.event.target.href)
-    console.log(window.location.pathname);
+    // console.log(window.location.pathname);
 
     const path = window.location.pathname
     const route = routes[path]
@@ -17,11 +18,12 @@ const route = async () => {
     const response = await fetch(route)
     const html = await response.text()
 
-    console.log(path);
+    // console.log(path);
 
     document.getElementById('root').innerHTML = html
 
-    console.log(html);
+    // console.log(html);
+
 }
 
 window.route = route;
